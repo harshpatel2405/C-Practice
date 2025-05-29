ba17d74b84afc3989c74406e5882cd6342eb7606
harshpatel2405 <harshpatel4504@gmail.com>
2025-05-29 18:22:36 +0530

    Variables

--- Variables/Variables.c
+++ Variables/Variables.c
@@ -1,6 +1,27 @@
-#include<stdio.h>
+/*
+    Name : Harsh Patel
+    Program Name : variable and its naming convention...
+    DOC : 29th May 2025
+*/
+
+#include <stdio.h>
 
 int main()
 {
-    printf("Hello World!");
+    int a, b;            // valid variable names
+    float $b = 14, _new; // valid variables name
+
+    // int 1name , #name, 96name ; --> invalid variable name
+
+    // Variable declaration and initialization
+    int age = 21;
+    float height = 5.9;
+    char grade = 'A';
+
+    // Assigning variables for values of variables
+    float length = 5.5;
+    float width = 3.2;
+    float area = length * width;
+
+    printf("Value is: %f", $b);
 }
